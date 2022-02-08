f3254b71dc99dc4fab7432a79542b3aabc5f273e
mobrien13 <mobrien13@ycp.edu>
2022-02-07 20:01:33 -0500

    Fresh Start
    Cleaned out default code for React.js

--- paper-trader/src/App.js
+++ paper-trader/src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello world - Mitchell</p>
-        <p>Hello World - Matthew</p>
-        <p>Hello World - Duncan</p>
-        <p>Hello World - Bryce</p>
-      </header>
-    </div>
+    <p>Hello World</p>
   );
 }
 
