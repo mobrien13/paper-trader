456182a34d0571902f701d1ed1236a4963a1256c
mobrien13 <mobrien13@ycp.edu>
2022-02-09 18:07:00 -0500

    Added Logo

--- paper-trader/src/App.js
+++ paper-trader/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import logo from './icons/logo.svg';
 
 function App() {
   return (
+    <>
     <body>
-
+      <img className = "logo" src={logo}/>
     </body>
-    
+
+    </>
   );
 }
 
