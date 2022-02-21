76b205cee2f0c4be4aa1d507ff0e4d855f5232be
jkeller11 <98911883+jkeller11@users.noreply.github.com>
2022-02-21 13:36:23 -0500

    Stage 1 of implementation
    Imported place holder svg for graph
    and setting up watchlist

--- paper-trader/src/Components/Pages/Dashboard.js
+++ paper-trader/src/Components/Pages/Dashboard.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './Dashboard.css';
 import './Pages.css';
+import graph from './graph.svg';
 
 function Dashboard() {
     return (
       <>
-        <h1>Dashboard</h1>
+        <div className='graph'>
+          <img src ={graph}></img>
+        </div>
+
+        <div className='watchlist'>
+          <div>
+            <container>
+              Stock: APPL
+              Price: $135
+            </container>
+          </div>
+          <div>
+            <container>
+              Stock: APPL
+              Price: $135
+            </container>
+          </div>
+          <div>
+            <container>
+              Stock: APPL
+              Price: $135
+            </container>
+          </div>
+        </div>
+      
+
       </>
     );
   }
   
-  
   export default Dashboard;
+
+function container(props){
+  return(
+    <p className='rectangle'>{props.children}</p>
+  );
+}
