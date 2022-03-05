dc4b0b70bf3cb6bb6feefa755c0c0728ee55fa80
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-03-05 16:35:48 -0500

    deleted unnecessary stuff

--- paper-trader/src/Components/Pages/Stock.js
+++ paper-trader/src/Components/Pages/Stock.js
@@ -2,11 +2,6 @@ import { React, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Stock = (props) => {
-    //const location = useLocation;
-    // const [pageURL, setPageURL] = useState(0);
-    // useEffect(() => {
-    //     setPageURL(window.location.pathname.substring(7));
-    // })
     const location = useLocation();
     const ticker = location.pathname.substring(7);
 
