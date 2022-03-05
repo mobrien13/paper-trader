d4ac1abbfd80718e144930d89b2b46dba39512b2
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-03-05 12:40:42 -0500

    responsive navbar completed

--- paper-trader/src/Components/Button/Button.js
+++ paper-trader/src/Components/Button/Button.js
@@ -1,12 +1,12 @@
 import './Button.css'
 import React from "react"
 
-const STYLES =[
+const STYLES = [
   "btn--primary--solid",
   "btn--primary--outline"
 ]
 
-const SIZES =["btn--medium, btn--small"]
+const SIZES = ["btn--medium, btn--small"]
 
 const Button = ({
   children,
@@ -15,19 +15,21 @@ const Button = ({
   buttonStyle,
   buttonSize,
 }) => {
- 
-  const checkButtonStyle = STYLES.includes(buttonStyle) 
+
+  const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
-    
-  const checkButtonSize = STYLES.includes(buttonSize) 
+
+  const checkButtonSize = STYLES.includes(buttonSize)
     ? buttonSize
     : SIZES[0];
 
   return (
-    <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick ={onClick} type={type}>
-      {children}
-    </button> 
+    <li>
+      <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+        {children}
+      </button>
+    </li>
   )
 }
 
--- paper-trader/src/Components/Navbar/Navbar.js
+++ paper-trader/src/Components/Navbar/Navbar.js
@@ -1,16 +1,40 @@
+import { React, useState } from 'react';
 import './Navbar.css';
 import logo from '../../icons/logo.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Button from '../../Components/Button/Button.js';
 
-function Navbar(props){
-    return(
-      <nav className = "navbar">
-        <Link to='/'><img className="logo" src={logo}/></Link>
-        <ul className = "navbar-buttons"> { props.children } </ul>
+function Navbar(props) {
+
+  //mobile menu open/setOpen
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to='/'><img className="logo" src={logo} /></Link>
+        <ul className="navbar-buttons"> {props.children} </ul>
         <ul className="navbar-links">
         </ul>
       </nav>
-    );
-  }
-  
-  export default Navbar;
+
+
+      <nav className='mobileNav'>
+        <div className='mobileTopBar'>
+          <Link to='/'><img className="logo" src={logo} /></Link>
+          <button id='hamburger' onClick={ () => setOpen(!open) }><i className='fa fa-bars'></i></button>
+        </div>
+        <ul className='navbar-buttons'>
+          {open &&
+            <div className='openMobileNav'>
+              <br /><br /><br /><br /><br />
+              {props.children}
+            </div>
+          }
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
