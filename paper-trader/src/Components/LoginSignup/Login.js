ecfd17dfd1264cdad5ab61972b39c90e3d71ba3b
mobrien13 <mobrien13@ycp.edu>
2022-03-05 21:40:10 -0500

    Log In / Sign Up
    Log in sign up work however the website does not update accordingly. Going to work with Mitch on getting the logic to work properly

--- paper-trader/src/Components/LoginSignup/Login.js
+++ paper-trader/src/Components/LoginSignup/Login.js
@@ -1,9 +1,36 @@
 import './Login.css';
+import {login, useAuth } from '../../firebase'
+import Button from '../Button/Button'
+import { useRef, useState } from 'react';
 
 function Login(){
+    const [ loading, setLoading] = useState(false);
+    const currentUser = useAuth();
+    
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    async function handleSignup ()  {
+        try{
+            setLoading(true);
+            await login(emailRef.current.value, passwordRef.current.value)
+        } catch{
+            alert("Error!")
+        }
+        setLoading(false);
+    }
+
     return(
         <>
         <h1>Log In</h1>
+        <div>
+            <input ref = {emailRef} placeholder = "Email" />
+            <input ref = {passwordRef} type = "password" placeholder = "Password" />
+        </div>
+
+        <Button disable = {loading} onClick={handleSignup}>Log In</Button>
+        
+
         </>
     );
   }
