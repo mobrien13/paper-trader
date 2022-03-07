9f2136b4041d226fbec04dca0b9392b4bec12ffc
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-03-06 21:01:56 -0500

    certain pages are not shown if not logged in
    really bad code which hides pages when users arent logged in. will need to make it better later

--- paper-trader/src/Components/Views/Views.js
+++ paper-trader/src/Components/Views/Views.js
@@ -7,24 +7,53 @@ import Settings from "../Pages/Settings";
 import { Routes, Route } from "react-router-dom";
 import Modal from '../Modal/Modal';
 import Stock from '../Pages/Stock';
+import { useAuth } from '../../firebase.js';
 
 //function
 function Views() {
+    const currentUser = useAuth();
+
+    const dashboard = () => {
+        if(currentUser!=null){
+            return (<Dashboard></Dashboard>)
+        }
+        else{
+            return (<h1>Please Log In Before Using the Dashboard</h1>)
+        }
+    }
+    const settings = () => {
+        if(currentUser!=null){
+            return (<Settings></Settings>)
+        }
+        else{
+            return (<h1>Please Log In Before Using Settings</h1>)
+        }
+    }
+    const stock = () => {
+        if(currentUser!=null){
+            return <Stock></Stock>;
+        }
+        else{
+            return <h1>Please Log In Before Using the Stock Pages</h1>
+        }
+    }
+
     return (
-    <>
-    {/*Routes the pages*/}
-    <Routes>
-        <Route index element={<Welcome></Welcome>}/>
-        <Route path='/welcome' element={<Welcome></Welcome>}/>
-        <Route path='/dashboard' element={<Dashboard></Dashboard>}/>
-        <Route path='/settings' element={<Settings></Settings>}/>
-        <Route path='/stock/*' element={<Stock></Stock>}/>
-        <Route path='*' element={<div><h1>404<br/>Error, Page Not Found</h1></div>}/>
-    </Routes>
-    </>
+        <>
+            {/*Routes the pages*/}
+            <Routes>
+                <Route index element={<Welcome></Welcome>} />
+                <Route path='/welcome' element={<Welcome></Welcome>} />
+                
+                <Route path='/dashboard' element={ dashboard() } />
+                <Route path='/settings' element={ settings() } />
+                <Route path='/stock/*' element={ stock() } />
+
+                <Route path='*' element={<div><h1>404<br />Error, Page Not Found</h1></div>} />
+            </Routes>
+        </>
     );
-  }
-  
-  
-  export default Views;
-  
+}
+
+
+export default Views;
