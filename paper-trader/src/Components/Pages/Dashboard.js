91d28d4645135117e25e9671c2aef5a89168e157
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-03-25 19:37:58 -0400

    dashboard styling changes

--- paper-trader/src/Components/Pages/Dashboard.js
+++ paper-trader/src/Components/Pages/Dashboard.js
@@ -13,51 +13,58 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 */
 
 function Dashboard() {
-    return (
-      <>
-        <div className='backround'>
+  return (
+    <>
+      <div className='backround'>
+
+        <div className='graphAndWatchlist'>
+
           <div className='graph-box'>
 
-  {/*generates graph from sparkline data, this will need to be changed for graph */}
+            {/*generates graph from sparkline data, this will need to be changed for graph */}
+
+
             <div className='graph'>
-              <Sparklines data={[5, 10, 5, 18, 20, 8, 15,12]} width={100} height={30} margin={5} >
-                <SparklinesLine color="black" style={{ fill: "none}"}}/>
+              <Sparklines data={[5, 10, 5, 18, 20, 8, 15, 12]} width={100} height={30} margin={5} >
+                <SparklinesLine color="black" style={{ fill: "none}" }} />
               </Sparklines>
             </div>
           </div>
-        
-           {/* Watchlist placeholder*/}
-          <ScrollList title="Watch List" stockName="APPL" price = "$135" upDown ="+4%" data ={[5, 10, 5, 18, 20, 8, 15, 12, 4, 21]}> </ScrollList>
-         
-          {/* holdings placeholder*/}
-          <div className='holdings'>
-            <div className='holdings-title'>
-              Current Holdings:
-            </div>
-            <div className='holdings-body'>
-              <ul>
-                <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
-                <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
-                <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
-              </ul>
-            </div>
+
+          {/* Watchlist placeholder*/}
+          <ScrollList title="Watch List" stockName="APPL" price="$135" upDown="+4%" data={[5, 10, 5, 18, 20, 8, 15, 12, 4, 21]}> </ScrollList>
+
+        </div>
+
+        {/* holdings placeholder*/}
+        <div className='holdings'>
+          <div className='holdings-title'>
+            Current Holdings:
           </div>
-          
-          {/* News placeholder*/}
-          <div className='news'>
-            <div className='list-title'>News</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-              <div className='news-item'>APPL PPRICE: $135</div>
-            </div>
-        </div>   
-      </>
-    );
-  }
-  
-  export default Dashboard;
+          <div className='holdings-body'>
+            <ul>
+              <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
+              <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
+              <li>COMPANY <span> PRICE </span><span> HOLDING </span></li>
+            </ul>
+          </div>
+        </div>
+
+        {/* News placeholder*/}
+        <div className='news'>
+          <div className='list-title'>News</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+          <div className='news-item'>APPL PPRICE: $135</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
