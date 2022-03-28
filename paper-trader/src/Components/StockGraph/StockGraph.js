5ae4c8da2c743b7bc140019f3fd6fff68ee6fc78
jkeller11 <98911883+jkeller11@users.noreply.github.com>
2022-03-27 21:14:14 -0400

    Fixed Graph State Change

--- paper-trader/src/Components/StockGraph/StockGraph.js
+++ paper-trader/src/Components/StockGraph/StockGraph.js
@@ -11,7 +11,7 @@ class StockGraph extends Component {
     super(props);
     this.state = { dataPoints1: [], dataPoints2: [], isLoaded: false };
   }
- 
+
   componentDidMount() {
 
     fetch("https://api.tdameritrade.com/v1/marketdata/"+this.props.ticker+"/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=year&period=2&frequencyType=daily&needExtendedHoursData=false")
@@ -37,14 +37,52 @@ class StockGraph extends Component {
 
           
 
+          this.setState({
+            isLoaded: true,
+            dataPoints1: dps1,
+            dataPoints2: dps2,
+            
+          });
+        }
+      )
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.ticker !== this.props.ticker){
+      
+      fetch("https://api.tdameritrade.com/v1/marketdata/"+this.props.ticker+"/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=year&period=2&frequencyType=daily&needExtendedHoursData=false")
+      .then(res => res.json())
+      .then(
+        (data) => {
+          var dps1 = [], dps2 = [];
+
+
+          for (var i = 0; i < data.candles.length; i++) {
+            dps1.push({
+              x: new Date(data.candles[i].datetime),
+              y: [
+                Number(data.candles[i].open),
+                Number(data.candles[i].high),
+                Number(data.candles[i].low),
+                Number(data.candles[i].close)
+              ]
+            });
+
+            dps2.push({x: new Date(data.candles[i].datetime), y: Number(data.candles[i].close)});
+          }
+
+          
 
           this.setState({
             isLoaded: true,
             dataPoints1: dps1,
             dataPoints2: dps2,
+            
           });
         }
       )
+    }
+    
   }
  
   render() {
