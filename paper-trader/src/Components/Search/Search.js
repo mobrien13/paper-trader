c3f38f680819a3ecd491b2b30a6899e6ce7e650f
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-04-20 14:53:12 -0400

    improved stock does not exist

--- paper-trader/src/Components/Search/Search.js
+++ paper-trader/src/Components/Search/Search.js
@@ -1,4 +1,4 @@
-import { React, useState} from 'react'
+import { React, useState } from 'react'
 import './Search.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,42 +8,45 @@ const Search = () => {
     const [ticker, setTicker] = useState('');
 
     const handleKeyPress = (event) => {
-        if(event.key === 'Enter'){
+        if (event.key === 'Enter') {
             //anything to do with the input submit goes here
-    
+
             //logs the keypress
             console.log('key pressed');
-    
+
             //clears input
             //var ticker = document.getElementById('searchInput').value;
-            //document.getElementById('searchInput').value ='';
+            document.getElementById('searchInput').value ='';
             
-            fetch(fetch("https://api.tdameritrade.com/v1/marketdata/"+ticker+"/quotes?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD")
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    const isEmpty = Object.keys(data).length
+            //renders page
+            routeChange(ticker);
 
-                    if(isEmpty !== 0){
-                       //renders page
-                       routeChange(ticker); 
-                    }
-                    else{
-                       alert("invalid ticker")
-                       document.getElementById('searchInput').value ='';
-                    }
-                }
-            )
-        )        
+            // fetch(fetch("https://api.tdameritrade.com/v1/marketdata/" + ticker + "/quotes?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD")
+            //     .then(res => res.json())
+            //     .then(
+            //         (data) => {
+            //             const isEmpty = Object.keys(data).length
+
+            //             if (isEmpty !== 0) {
+            //                 //renders page
+            //                 routeChange(ticker);
+            //             }
+            //             else {
+            //                 alert("invalid ticker")
+            //                 document.getElementById('searchInput').value = '';
+            //             }
+            //         }
+            //     )
+            // )
+        }
     }
-}    
 
     //useNavigate hook
     let navigate = useNavigate();
 
     //routes the page
     //change route to check if stock is valid stock
-    const routeChange = (ticker) =>{ 
+    const routeChange = (ticker) => {
         navigate('stock/' + ticker);
     }
 
@@ -51,7 +54,7 @@ const Search = () => {
         <>
             <li>
                 {/* //generates search input and runs handleKeyPress */}
-                <input id='searchInput' placeholder='Enter Symbol' onKeyPress={ handleKeyPress } onChange={event => setTicker(event.target.value)}></input>
+                <input id='searchInput' placeholder='Enter Symbol' onKeyPress={handleKeyPress} onChange={event => setTicker(event.target.value)}></input>
             </li>
         </>
     )
