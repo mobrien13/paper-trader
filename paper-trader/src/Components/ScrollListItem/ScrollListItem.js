335c2972fd46d9de9fb6311a74eadd0d70b7851f
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-04-26 20:43:32 -0400

    watchlist items link to stocks

--- paper-trader/src/Components/ScrollListItem/ScrollListItem.js
+++ paper-trader/src/Components/ScrollListItem/ScrollListItem.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import { usersDatabase } from '../../fakeDatabase.js';
 import { getUserWatchList, setUserWatchList } from '../../firebase';
+import { Link } from 'react-router-dom';
 
 
 const ScrollListItem = (props) => {
@@ -12,14 +13,14 @@ const ScrollListItem = (props) => {
     //real database
     const [watchlist, setWatchlist] = useState([]);
 
-     // Watchlist data
-     const [price,setPrice] = useState(0);
-     const [upDown, setUpDown] = useState(0);
-     const [closes] = useState([]);
+    // Watchlist data
+    const [price, setPrice] = useState(0);
+    const [upDown, setUpDown] = useState(0);
+    const [closes] = useState([]);
 
 
     //function to remove item from watchlist
-    async function removeItem()  {
+    async function removeItem() {
         // watchlist = removeByValue(props.stockName);
 
         //get watchlist (async)
@@ -43,45 +44,50 @@ const ScrollListItem = (props) => {
 
     useEffect(() => {
         // Grapbs last 10 days of data for upDown and sparkline and upDown
-        fetch("https://api.tdameritrade.com/v1/marketdata/"+props.stockName+"/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=month&period=1&frequencyType=daily&frequency=1&needExtendedHoursData=false")
-        .then(res => res.json())
-        .then(
-        (data) => {
-                for (let i = data.candles.length -1; i >= data.candles.length-10; i--) {
-                    closes.push(Number(data.candles[i].close));
-                    
+        fetch("https://api.tdameritrade.com/v1/marketdata/" + props.stockName + "/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=month&period=1&frequencyType=daily&frequency=1&needExtendedHoursData=false")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    for (let i = data.candles.length - 1; i >= data.candles.length - 10; i--) {
+                        closes.push(Number(data.candles[i].close));
+
+                    }
+
+                    Array.prototype.reverse.call(closes);
+
+                    setUpDown((((closes[closes.length - 1] - closes[0]) / closes[0]) * 100).toFixed(2));
                 }
-
-                Array.prototype.reverse.call(closes);
-
-                setUpDown((((closes[closes.length-1]-closes[0]) / closes[0]) * 100 ).toFixed(2));
-            }
-        )
+            )
 
         //Price is current minutes close
-        fetch("https://api.tdameritrade.com/v1/marketdata/"+props.stockName+"/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=day&period=3&frequencyType=minute&frequency=1&needExtendedHoursData=false")
-        .then(res => res.json())
-        .then(
-            (data) => {
-                setPrice((Number(data.candles[data.candles.length-1].close)).toFixed(2));
-            }
-        )
-      },[]);
-    
-    
-   
+        fetch("https://api.tdameritrade.com/v1/marketdata/" + props.stockName + "/pricehistory?apikey=LSVZWEQEHTTZGGWUYS1ZKNA0OAQCCVDD&periodType=day&period=3&frequencyType=minute&frequency=1&needExtendedHoursData=false")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setPrice((Number(data.candles[data.candles.length - 1].close)).toFixed(2));
+                }
+            )
+    }, []);
+
+
+
 
     return (
         <>
 
             <div className='list-body'>
+
                 {/* // gives name and price to stock  */}
-                <div>{props.stockName} <br></br>{"$"+price}</div>
+                <Link to={'/stock/' + props.stockName} className='scrollListLink'>
+                    <div>{props.stockName}<br></br>{"$" + price}</div>
+                </Link>
                 {/* //up and down prices */}
-                <div>Up/Down<br></br>{upDown+"%"}</div>
+                <Link to={'/stock/' + props.stockName} className='scrollListLink'>
+                    <div>Up/Down<br></br>{upDown + "%"}</div>
+                </Link>
                 {/* //generates graph */}
                 <Sparklines data={closes} width={100} height={30} limit={8}>
-                    <SparklinesLine color="black" style={{ fill: "none}" }} />
+                    <SparklinesLine color="white" style={{ fill: "none}" }} />
                 </Sparklines>
                 <div><i onClick={() => removeItem()} className="fa fa-minus-circle" aria-hidden="true"></i></div>
             </div>
