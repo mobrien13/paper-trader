a9db4ece62e661c8679d36bb28e463fa213a3c5f
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-04-29 00:54:02 -0400

    combined css changes

--- paper-trader/src/Components/PortfolioGraph/PortfolioGraph.js
+++ paper-trader/src/Components/PortfolioGraph/PortfolioGraph.js
@@ -1,82 +1,119 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import CanvasJSReact from '../../canvasjs.stock.react';
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
 
+CanvasJS.addColorSet("colors",
+  [//colorSet Array 
+    "#06F",     //navigator integral color
+  ]);
 
 
 class PortfolioGraph extends Component {
   render() {
     const options = {
-      theme:"light1",
+      theme: "light1",
       animationEnabled: "True",
-      animationDuration: 1200,      
+      animationDuration: 1200,
+      colorSet:"colors",
+
+
 
       charts: [{
-          xaxis:{
-            lineThickness: 3,
-            tickLength: 0,
-            
-            crosshair: {
-              enabled: true,
-              snapToDataPoint: true,
-            }
-          },
-         
-          axisY: {
-            lineThickness: 3,
-            title: "Portfolio Value USD",
-            prefix: "$",
-            tickLength: 0
-          },
+        xaxis: {
+          lineThickness: 3,
+          tickLength: 0,
+          lineColor: "#242526",
+          tickColor: "#242526",
+          titleFontColor: "#242526",
+          gridColor: "#242526",
           crosshair: {
             enabled: true,
             snapToDataPoint: true,
-          },
+          }
+        },
+        axisY: {
+          lineThickness: 1,
+          title: "Portfolio Value USD",
+          prefix: "$",
+          tickLength: 0,
+          lineColor: "#242526",
+          tickColor: "#242526",
+          titleFontColor: "#242526",
+          gridColor: "#242526",
+        },
+        crosshair: {
+          enabled: true,
+          snapToDataPoint: true,
+        },
 
 
-          data: [{
-            //"CSS" like settigns for graph
-            type: "spline",
-            markerType:"none",
-            lineThickness: "2.8",
-            lineColor:"#707C8F",
-            fillOpacity: .3,
-            dataPoints: [
-	      { x: new Date("2018-01-01"), y: 100 },
-	      { x: new Date("2018-02-01"), y: 340 },
-	      { x: new Date("2018-03-01"), y: 220 },
-	      { x: new Date("2018-04-01"), y: 270 },
-	      { x: new Date("2018-05-01"), y: 300 },
-	      { x: new Date("2018-06-01"), y: 370 },
-	      { x: new Date("2018-07-01"), y: 290 },
-	      { x: new Date("2018-08-01"), y: 290 },
-	      { x: new Date("2018-09-01"), y: 300 },
-	      { x: new Date("2018-10-01"), y: 140 },
-	      { x: new Date("2018-11-01"), y: 300 },
-	      { x: new Date("2018-12-01"), y: 290 },
-	      { x: new Date("2019-01-01"), y: 270 },
-	      { x: new Date("2019-02-01"), y: 190 },
-	      { x: new Date("2019-03-01"), y: 140 },
-	      { x: new Date("2019-04-01"), y: 290 },
-	      { x: new Date("2019-05-01"), y: 140 },
-	      { x: new Date("2019-06-01"), y: 360 },
-	      { x: new Date("2019-07-01"), y: 290 },
-	      { x: new Date("2019-08-01"), y: 300 },
-	      { x: new Date("2019-09-01"), y: 200 },
-	      { x: new Date("2019-10-01"), y: 300 },
-	      { x: new Date("2019-11-01"), y: 140 },
-	      { x: new Date("2019-12-01"), y: 190 }
-	  ]
-         }]
+        data: [{
+          //"CSS" like settigns for graph
+          type: "spline",
+          markerType: "none",
+          lineThickness: "2.8",
+          lineColor: "#06F",
+          fillOpacity: .5,
+          dataPoints: [
+            { x: new Date("2018-01-01"), y: 100 },
+            { x: new Date("2018-02-01"), y: 340 },
+            { x: new Date("2018-03-01"), y: 220 },
+            { x: new Date("2018-04-01"), y: 270 },
+            { x: new Date("2018-05-01"), y: 300 },
+            { x: new Date("2018-06-01"), y: 370 },
+            { x: new Date("2018-07-01"), y: 290 },
+            { x: new Date("2018-08-01"), y: 290 },
+            { x: new Date("2018-09-01"), y: 300 },
+            { x: new Date("2018-10-01"), y: 140 },
+            { x: new Date("2018-11-01"), y: 300 },
+            { x: new Date("2018-12-01"), y: 290 },
+            { x: new Date("2019-01-01"), y: 270 },
+            { x: new Date("2019-02-01"), y: 190 },
+            { x: new Date("2019-03-01"), y: 140 },
+            { x: new Date("2019-04-01"), y: 290 },
+            { x: new Date("2019-05-01"), y: 140 },
+            { x: new Date("2019-06-01"), y: 360 },
+            { x: new Date("2019-07-01"), y: 290 },
+            { x: new Date("2019-08-01"), y: 300 },
+            { x: new Date("2019-09-01"), y: 200 },
+            { x: new Date("2019-10-01"), y: 300 },
+            { x: new Date("2019-11-01"), y: 140 },
+            { x: new Date("2019-12-01"), y: 190 }
+          ]
+        }]
       }],
+
+
       navigator: {
         slider: {
           //minimum: new Date("2018-07-01"),
           //maximum: new Date("2019-06-30")
         }
+      },
+
+      rangeSelector: {
+        selectedRangeButtonIndex: 5,
+        buttonStyle: {
+          backgroundColor: "#ffffff",
+          backgroundColorOnHover: "#06f",
+          backgroundColorOnSelect: "#06f",
+          borderColor: "#06f",
+          labelFontColor: "#242526",
+          labelFontColorOnHover: "#ffffff",
+        },
+        inputFields: {
+          style: {
+            borderColor: "#06f",
+            borderColorOnFocus: "#06f",
+            fontColor: "#242526",
+          }
+        },
       }
+
     };
+
+
     const containerProps = {
       width: "100%",
       height: "60vh",
@@ -86,7 +123,7 @@ class PortfolioGraph extends Component {
       <div>
         <CanvasJSStockChart
           options={options}
-          containerProps = {containerProps}
+          containerProps={containerProps}
           onRef={ref => this.stockChart = ref}
         />
       </div>
