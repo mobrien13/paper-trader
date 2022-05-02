f43ceccf072dfc80085a2410b3f66f17e0717fbe
mobrien13 <mobrien13@ycp.edu>
2022-05-02 14:48:30 -0400

    Update Dashboard.js

--- paper-trader/src/Components/Pages/Dashboard.js
+++ paper-trader/src/Components/Pages/Dashboard.js
@@ -71,8 +71,8 @@ function Dashboard() {
         <Orders>
           {/* for now it has both open and closed orders. in the future, it should just show the open orders and have a separate menu to so the receipts */}
           <ul>
-            {userHoldings.map((item) => !item.isSold && <li className='ordersItem'>{item.ticker.toUpperCase()}: {item.quantity} Shares @ ${item.buyPrice}/share</li>)}
-            {userHoldings.map((item) => item.isSold && <li className='ordersItem'>{item.ticker.toUpperCase()}: {item.quantity} Shares @ ${item.buyPrice}/share Sold @ ${item.sellPrice}/share</li>)}
+            {/* {userHoldings.map((item) => !item.isSold && <li className='ordersItem'>{item.ticker.toUpperCase()}: {item.quantity} Shares @ ${item.buyPrice}/share</li>)}
+            {userHoldings.map((item) => item.isSold && <li className='ordersItem'>{item.ticker.toUpperCase()}: {item.quantity} Shares @ ${item.buyPrice}/share Sold @ ${item.sellPrice}/share</li>)} */}
           </ul>
         </Orders>
 
