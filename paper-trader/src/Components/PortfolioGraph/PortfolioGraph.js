1099bbf252b815b10aead008033387eb4c3a0f5c
jkeller11 <98911883+jkeller11@users.noreply.github.com>
2022-05-06 17:40:47 -0400

    test

--- paper-trader/src/Components/PortfolioGraph/PortfolioGraph.js
+++ paper-trader/src/Components/PortfolioGraph/PortfolioGraph.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import CanvasJSReact from '../../canvasjs.stock.react';
+import {getHoldings, getPastOrders} from '../../firebase';
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
+
+
+
+// { x: new Date("2018-02-01"), y: 340 },
+// { x: new Date("2018-03-01"), y: 220 },
+// { x: new Date("2018-04-01"), y: 270 },
+// { x: new Date("2018-05-01"), y: 300 },
+// { x: new Date("2018-06-01"), y: 370 },
+// { x: new Date("2018-07-01"), y: 290 },
+// { x: new Date("2018-08-01"), y: 290 },
+// { x: new Date("2018-09-01"), y: 300 },
+// { x: new Date("2018-10-01"), y: 140 },
+// { x: new Date("2018-11-01"), y: 300 }
 
 CanvasJS.addColorSet("colors",
   [//colorSet Array 
@@ -10,6 +24,53 @@ CanvasJS.addColorSet("colors",
 
 
 class PortfolioGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoaded: false, data:[] };
+  }
+
+  componentDidMount() {
+   let current = []
+  // let past = []
+   
+
+
+
+
+    getHoldings().then((result) => {
+      for (let i = 1; i <= result.length; i++){
+        current.push({x: new Date((new Date (result[i].timebought)).setHours(0,0,0,0)), y: (result[i].buyPrice * result[i].quantity)})
+      }
+
+      let prev = current[0].x
+      for(let i = 1; i <= current.length; i++){
+        let next = current[i].x
+        
+        if(prev === next){
+          current[i].y = current[i-1].y + current[i].y 
+          current.splice(i-1,1)
+        }
+
+        prev = next
+      }
+
+     //.toDateString()
+      
+    })
+
+  
+    
+    
+    
+
+
+
+    this.setState({
+      isLoaded: true,
+      data: current
+    });
+  }
+
   render() {
     const options = {
       theme: "light1",
@@ -55,32 +116,7 @@ class PortfolioGraph extends Component {
           lineThickness: "2.8",
           lineColor: "#06F",
           fillOpacity: .5,
-          dataPoints: [
-            { x: new Date("2018-01-01"), y: 100 },
-            { x: new Date("2018-02-01"), y: 340 },
-            { x: new Date("2018-03-01"), y: 220 },
-            { x: new Date("2018-04-01"), y: 270 },
-            { x: new Date("2018-05-01"), y: 300 },
-            { x: new Date("2018-06-01"), y: 370 },
-            { x: new Date("2018-07-01"), y: 290 },
-            { x: new Date("2018-08-01"), y: 290 },
-            { x: new Date("2018-09-01"), y: 300 },
-            { x: new Date("2018-10-01"), y: 140 },
-            { x: new Date("2018-11-01"), y: 300 },
-            { x: new Date("2018-12-01"), y: 290 },
-            { x: new Date("2019-01-01"), y: 270 },
-            { x: new Date("2019-02-01"), y: 190 },
-            { x: new Date("2019-03-01"), y: 140 },
-            { x: new Date("2019-04-01"), y: 290 },
-            { x: new Date("2019-05-01"), y: 140 },
-            { x: new Date("2019-06-01"), y: 360 },
-            { x: new Date("2019-07-01"), y: 290 },
-            { x: new Date("2019-08-01"), y: 300 },
-            { x: new Date("2019-09-01"), y: 200 },
-            { x: new Date("2019-10-01"), y: 300 },
-            { x: new Date("2019-11-01"), y: 140 },
-            { x: new Date("2019-12-01"), y: 190 }
-          ]
+          dataPoints: this.state.data
         }]
       }],
 
@@ -121,11 +157,14 @@ class PortfolioGraph extends Component {
     };
     return (
       <div>
-        <CanvasJSStockChart
+        {this.state.isLoaded &&
+          <CanvasJSStockChart
           options={options}
           containerProps={containerProps}
           onRef={ref => this.stockChart = ref}
         />
+        }
+        
       </div>
     );
   }
