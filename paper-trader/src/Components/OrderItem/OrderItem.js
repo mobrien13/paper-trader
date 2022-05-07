bdbc6b6194e178422690bd2f0cd40b20e228bdbd
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-05-07 16:45:50 -0400

    fixed order styling on mobile

--- paper-trader/src/Components/OrderItem/OrderItem.js
+++ paper-trader/src/Components/OrderItem/OrderItem.js
@@ -10,24 +10,24 @@ const OrderItem = (props) => {
             {props.type === "order" &&
                 <>
                     <p>{props.ticker}</p>
-                    <p>${props.buyPrice.toFixed(2)}/share</p>
+                    <p className='hiddenOnMobile'>${props.buyPrice.toFixed(2)}/share</p>
                     <p>{props.quantity} shares</p>
-                    <p>${props.cost.toFixed(2)}</p>
-                    <p>{new Date(props.date).toDateString()}</p>
+                    <p className='hiddenOnMobile'>${props.cost.toFixed(2)}</p>
+                    <p className='hiddenOnMobile'>{new Date(props.date).toDateString()}</p>
                     <p>{props.quantitySold} shares</p>
-                    <p>${props.sellPrice.toFixed(2)}</p>
-                    <p>{props.sellDate !== null && new Date(props.sellDate).toDateString()}{props.sellDate === null && "N/A"}</p>
+                    <p className='hiddenOnMobile'>${props.sellPrice.toFixed(2)}</p>
+                    <p className='hiddenOnMobile'>{props.sellDate !== null && new Date(props.sellDate).toDateString()}{props.sellDate === null && "N/A"}</p>
                 </>
             }
             {props.type === "receipt" &&
                 <>
                     <p>{props.ticker}</p>
-                    <p>${props.buyPrice.toFixed(2)}/share</p>
-                    <p>{props.quantity} shares</p>
+                    <p className='hiddenOnMobile'>${props.buyPrice.toFixed(2)}/share</p>
+                    <p className='hiddenOnMobile'>{props.quantity} shares</p>
                     <p>${props.cost.toFixed(2)}</p>
-                    <p>${props.sellPrice}</p>
+                    <p className='hiddenOnMobile'>${props.sellPrice}</p>
                     <p>${props.profit.toFixed(2)}</p>
-                    <p>{new Date(props.date).toDateString()}</p>
+                    <p className='hiddenOnMobile'>{new Date(props.date).toDateString()}</p>
                 </>
             }
         </>
